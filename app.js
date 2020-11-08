e0b9f3d57ebabccce89717be66d2bbe4314cf500
--global <ckca1234@gmail.com>
2020-11-08 21:19:37 +0100

    hmmm

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const fetch = require('node-fetch');
-// const mongoose = require('mongoose'); database giver errors 
+const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const port =  process.env.PORT||5500;
 const app = express();
 const hbs = require('hbs');
 
+//fetch / post
+fetch("https://krdo-joke-registry.herokuapp.com/api/services",
+ {form:{name:"dumbjokeservice", address:"https://dumbjokeservice.herokuapp.com/",
+  secret:"999"}});
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/public');
-let jokesUrl = 'indsæt link til vores cluster her';
+let jokesUrl = 'https://cloud.mongodb.com/v2/5f9a95b038b13109adf71659#clusters/detail/dumb-jokes-services';
 
 async function get(url) {
   const respons = await fetch(url);
@@ -18,14 +22,10 @@ async function get(url) {
       throw new Error(respons.status);
   return await respons.json();
 }
-// app.use(express.static(__dirname + '../FrontEnd/jokeservices.html'));
 
-
-
-app.use(express.static(__dirname+'public'));
+app.use(express.static(__dirname+'/public'));
 app.use(cors());
 app.use(express.json());
-
 app.get('/', async (request, response) => {
   try {
       let jokes = await get(jokesUrl);
@@ -42,38 +42,53 @@ app.get('/', async (request, response) => {
   }
 });
 
-fetch("https://krdo-joke-registry.herokuapp.com/api/services",
- {form:{name:"dumbjokeservice", address:"https://dumbjokeservice.herokuapp.com/",
-  secret:"123"}});
 
 
-/** Database giver errors udkommenteret
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://Fred:admin@dumb-jokes-services.woyzv.mongodb.net/test"
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+
+
+//Mongoose connection________________________________________________________________________________________________________________________
+const MONGODB_URI = 'mongodb+srv://fred:admin@dumb-jokes-services.woyzv.mongodb.net/test';
+mongoose.connect(MONGODB_URI || 'mongodb://localhost/JokeDB', { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
 });
-
-
-mongoose.connect('mongodb://localhost/JokeDB', { useNewUrlParser: true, useUnifiedTopology: true });
-// test om databasen er tændt
 const db = mongoose.connection;
-db.on('connection', () => {
-    console.log('Databasen er tændt');
+db.on('connected', () => {
+    console.log('mongoose is connected!');
 });
-*/
-/**
-async function main() {
-}
-main();
 
-// new shit 
-const connect = require("connect");
-const applet = connect().use(connect.static(__dirname + '/FrontEnd'));
-*/
+//Schema
+const Schema = mongoose.Schema;
+const JokeSchema = new Schema( {
+    setUp: String,
+    punchLine: String 
+});
+
+//Model
+const Joke = mongoose.model('Joke', JokeSchema);
+
+//Saving data to our mongo database
+const data = {
+    setUp: 'setup test 2',
+    punchLine: 'punchline test 2'
+};
+
+const newJoke = new Joke(data); //instance of the model
+
+//routes
+app.get('/api', (req, res) => {
+
+    Joke.find({ })
+    .then((data) => {
+        console.log('Data: ', data);
+        res.json(data);
+    })
+    .catch((error) => {
+        console.log('Error: ', error);
+    });
+
+});
+
 
 app.listen(port, () => {
     console.log('app is listening on port: ' + `${port}`);
