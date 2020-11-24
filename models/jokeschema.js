c54833b60c11356267906fe598259582654ab526
--global <ckca1234@gmail.com>
2020-11-24 01:01:57 +0100

    schema

---
+++ models/jokeschema.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const fetch = require('node-fetch');
+
+
+const joke = new mongoose.Schema({
+    setup:String,
+    punchline:String
+});
+
+const jokeModel = mongoose.model('jokes', joke);
